2b1ad2f4b0d629a6b66907c730767e1aa9e70fa8
rudzie-dev <249273869+rudzie-dev@users.noreply.github.com>
2026-05-25 21:06:58 +0200

    responsive layout + lucide icons

---
+++ sw.js
@@ -0,0 +1,34 @@
+const CACHE = 'todos-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/icon.png',
+  '/icon.svg'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting())
+  );
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys()
+      .then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request, { ignoreSearch: true })
+      .then(cached => cached || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }))
+      .catch(() => caches.match('/index.html'))
+  );
+});
